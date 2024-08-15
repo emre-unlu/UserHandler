47c52b275b895566cb149e1eb5767d7e8600cf46
emre <iemre2003@gmail.com>
2024-08-15 12:41:13 +0200

    Added: 1-Custom Validation to Update Password

--- internal/const.go
+++ internal/const.go
@@ -1,3 +1,9 @@
 package internal
 
 const BirthdayFormat = "2006-01-02"
+const (
+	SpecialChars = "!@#$%^&*()_+{}:<>?|[];',./`~"
+	Lowercase    = "abcdefghijklmnopqrstuvwxyz"
+	Uppercase    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	Numbers      = "0123456789"
+)
